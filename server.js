dbcd2d3cd2523260478b1d8caa6d19f2ff33ab94
ktapia576 <ktapia576@hotmail.com>
2020-06-09 23:46:43 -0400

    Change server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const hbs = require('hbs');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;  // Server port for app2
 
 app.set('view engine', 'hbs');
 app.use(express.static("public"));
